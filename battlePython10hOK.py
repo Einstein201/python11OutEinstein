d47a7142307fa92495549889bde1cf6d2301c046
Brunine Einstein <84164381+Einstein201@users.noreply.github.com>
2023-08-12 22:16:42 -0400

    Add files via upload

---
+++ battlePython10hOK.py
@@ -0,0 +1,11 @@
+teks ="HACK hello mwne anfom pa pu le la  "
+kntiteMo= len(teks.split())
+print(kntiteMo)
+
+
+if "hack" in teks.lower():
+    print ("gen Viris")
+
+trsf=teks.title()
+print (trsf)
+
